e28a7ed993ca5baedf4735cf57199d0c6fd9afc1
John Rom <john@nimblelight.com>
2019-11-16 17:52:20 -0500

    Delegate events to support multiple WYSIWYGs.

--- src/ckeditor5-oc-media.js
+++ src/ckeditor5-oc-media.js
@@ -8,41 +8,51 @@ import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 export class InsertImage extends Plugin {
     init() {
         const editor = this.editor;
+		const editorTextarea = $(editor.sourceElement);
+		const fieldWrapper = editorTextarea.closest('.ckeditor-field');
+		const modal = $('#mediaModalHtmlField');
 
         editor.ui.componentFactory.add( 'insertImage', locale => {
             const view = new ButtonView( locale );
 
             view.set( {
-                label: 'Insert image',
+                label: 'Insert Orchard Image',
                 icon: imageIcon,
                 tooltip: true
             } );
 
             // Callback executed once the image is clicked.
             view.on( 'execute', () => {
-				$("#mediaApp").detach().appendTo('#mediaModalHtmlField .modal-body');
-				$("#mediaApp").show();
 				mediaApp.selectedMedias = [];
-				var modal = $('#mediaModalHtmlField').modal();
 
-				$('#mediaHtmlFieldSelectButton').on('click', function (v) {
-					for (i = 0; i < mediaApp.selectedMedias.length; i++) {
-						editor.model.change( writer => {
-							const imageElement = writer.createElement( 'image', {
-								src: mediaApp.selectedMedias[i].mediaPath
-							} );
+				const mediaAppElement = $("#mediaApp");
+				const modalBody = modal.find('.modal-body');
 
-							// Insert the image in the current selection location.
-							editor.model.insertContent( imageElement, editor.model.document.selection );
-						} );
-					}
-					$(document).trigger('contentpreview:render');
-					$('#mediaModalHtmlField').modal('hide');
-					return true;
-				});
+				mediaAppElement
+					.appendTo(modalBody)
+					.show();
+				modal.appendTo(fieldWrapper);
+
+				modal.modal();
             } );
 
             return view;
         } );
+
+		fieldWrapper.on('click', '#mediaHtmlFieldSelectButton', function (v) {
+			for (i = 0; i < mediaApp.selectedMedias.length; i++) {
+				editor.model.change( writer => {
+					// todo, dynamically map real URL
+					const imageElement = writer.createElement( 'image', {
+						src: '/Media/' + mediaApp.selectedMedias[i].mediaPath
+					} );
+
+					// Insert the image in the current selection location.
+					editor.model.insertContent( imageElement, editor.model.document.selection );
+				} );
+			}
+			modal.modal('hide');
+			return true;
+		});
     }
 }
