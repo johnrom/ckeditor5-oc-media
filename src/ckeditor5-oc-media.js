1c71b5a2a09f2cab6077c6fdcbca9c06843e8c90
John Rom <web@johnrom.com>
2020-04-13 17:16:21 -0400

    Updating packages, eslinting

--- src/ckeditor5-oc-media.js
+++ src/ckeditor5-oc-media.js
@@ -1,3 +1,4 @@
+/* global $, mediaApp */
 /**
  * @module ckeditor5-oc-media/ckeditor5-oc-media
  */
@@ -6,53 +7,54 @@ import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 
 export class InsertImage extends Plugin {
-    init() {
-        const editor = this.editor;
-		const editorTextarea = $(editor.sourceElement);
-		const fieldWrapper = editorTextarea.closest('.ckeditor-field');
-		const modal = $('#mediaModalHtmlField');
+	init() {
+		const editor = this.editor;
+		const editorTextarea = $( editor.sourceElement );
+		const fieldWrapper = editorTextarea.closest( '.ckeditor-field' );
+		const modal = $( '#mediaModalHtmlField' );
 
-        editor.ui.componentFactory.add( 'insertImage', locale => {
-            const view = new ButtonView( locale );
+		editor.ui.componentFactory.add( 'insertImage', locale => {
+			const view = new ButtonView( locale );
 
-            view.set( {
-                label: 'Insert Orchard Image',
-                icon: imageIcon,
-                tooltip: true
-            } );
+			view.set( {
+				label: 'Insert Orchard Image',
+				icon: imageIcon,
+				tooltip: true
+			} );
 
-            // Callback executed once the image is clicked.
-            view.on( 'execute', () => {
+			// Callback executed once the image is clicked.
+			view.on( 'execute', () => {
 				mediaApp.selectedMedias = [];
 
-				const mediaAppElement = $("#mediaApp");
-				const modalBody = modal.find('.modal-body');
+				const mediaAppElement = $( '#mediaApp' );
+				const modalBody = modal.find( '.modal-body' );
 
-				mediaAppElement
-					.appendTo(modalBody)
-					.show();
-				modal.appendTo(fieldWrapper);
+				mediaAppElement.appendTo( modalBody ).show();
+				modal.appendTo( fieldWrapper );
 
 				modal.modal();
-            } );
+			} );
 
-            return view;
-        } );
+			return view;
+		} );
 
-		fieldWrapper.on('click', '#mediaHtmlFieldSelectButton', function (v) {
-			for (i = 0; i < mediaApp.selectedMedias.length; i++) {
+		fieldWrapper.on( 'click', '#mediaHtmlFieldSelectButton', function() {
+			for ( let i = 0; i < mediaApp.selectedMedias.length; i++ ) {
 				editor.model.change( writer => {
 					// todo, dynamically map real URL
 					const imageElement = writer.createElement( 'image', {
-						src: '/Media/' + mediaApp.selectedMedias[i].mediaPath
+						src: '/Media/' + mediaApp.selectedMedias[ i ].mediaPath
 					} );
 
 					// Insert the image in the current selection location.
-					editor.model.insertContent( imageElement, editor.model.document.selection );
+					editor.model.insertContent(
+						imageElement,
+						editor.model.document.selection
+					);
 				} );
 			}
-			modal.modal('hide');
+			modal.modal( 'hide' );
 			return true;
-		});
-    }
+		} );
+	}
 }
